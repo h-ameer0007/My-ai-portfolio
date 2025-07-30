3e2e7d5fd852348b2664640774c31b33488935a5
h-ameer0007 <H.ameer0007@gmail.com>
2025-07-30 21:30:50 +0200

    this is the biggest update yet. as now i have studied the error handling i have implimented the whole error handling. i have added nested loops to improve the fuctionality. now the program does not move forward with invalid option selection form menu or invalid input for first or second number. it keeps looping until the user provides a valid input for each of the input asked.

--- projects/capestone-projects/week1/cli-calculator/calculator.py
+++ projects/capestone-projects/week1/cli-calculator/calculator.py
@@ -1,110 +1,71 @@
-# a simple calculator made with python
-
-# defines the addition operation
-
-
-def is_valid_float_string(s):
-    if not s:
-        return False
-    if s.startswith("-"):
-        temp_s = s[1:]
-    else:
-        temp_s = s
-
-    dot_count = temp_s.count(".")
-
-    if dot_count <= 1 and temp_s.replace(".", "", 1).isdigit():
-        return True
-    else:
-        return False
-
-
+# CLI Calculator
+# defines add function
 def add(num1, num2):
-    return num1+num2
-
-# defines the subtraction operation
+    return num1 + num2
+# defines subtract function
 
 
 def subtract(num1, num2):
     return num1-num2
-
-# defines the multiplication operation
+# defines multiply function
 
 
 def multiply(num1, num2):
-    return num1*num2
-
-# defines the devision operation
+    return num1 * num2
+# defines decide function
 
 
 def devide(num1, num2):
     return num1/num2
 
 
-# an open loop that prints menu of selection and allows user to continue operations untill quit.
+# initiates the first loop to keep displaying the menu to the user.
 while True:
-    print("----Menu----")  # prints the menu lable
-    print("1.Add")        # prints the addition option
-    print("2.Subtract")   # prints the subtraction option
-    print("3.Multiply")   # prints the multiplication option
-    print("4.Devide")     # prints the devition option
-    print("5.Quit")       # prints the quit option
-    print("Select an option.")  # prints the selction operation line
-    # takes the user input from the selction menu
-    option_selection_str = input("")
-    # checks for if the input is a numeric value and if is in menu
-    if option_selection_str.isdigit() and (0 < int(option_selection_str) <= 5):
-        # the input is converted into an intiger
-        option_selection = int(option_selection_str)
-        if option_selection == 5:
-            # ptints the quiting message
-            print("Quiting the program... Goodbye.")
-            # breaks the loop
-            break
-        # asks for first number
-        while True:
-
-            First_number_str = input("First Number : ")
-            # checks if it is a numeric value
-            if is_valid_float_string(First_number_str):
-                # converts it into a number with decimal point
-                num1 = float(First_number_str)
+    print("----Menu----")
+    print("----1.add----")
+    print("----2.Subtract----")
+    print("----3.Multiply----")
+    print("----4.Devide----")
+    print("----5.Exit----")
+    try:  # starts a check for if the input is an integer and handles the invalid input
+        option_selection = int(
+            input("Please select an option 1-5. "))
+        if 1 <= option_selection <= 5:  # checks for is the user has selected from the menu
+            # exits the loop as soon as user selects the option without interpreting further code.
+            if option_selection == 5:
+                print("Exiting program. Bye!")
                 break
-              # if the value provided is other then a number
-            else:  # prints the error message
-                print("Please provide a number.")
-            # asks for second number
-        while True:
-
-            Second_number_str = input("Second Number : ")
-            # checks if it is a numeric value
-            if is_valid_float_string(Second_number_str):
-                # converts it into a number with decimal point
-                num2 = float(Second_number_str)
-                break
-            # if the value provided is other then a number
-            else:
-                print("Please provide a number.")  # prints the error message
-        # checks if the additoin option is selected
-        if int(option_selection) == 1:
-            # calls the addtion function and prints the result
-            print(f" >> {num1} + {num2} =", add(num1, num2), "<<")
-        elif int(option_selection) == 2:
-            # calls the subtraction function and prints the result
-            print(f" >> {num1} - {num2} =", subtract(num1, num2), "<<")
-        elif int(option_selection) == 3:
-            # calls the multiplication function and prints the result
-            print(f" >> {num1} * {num2} =", multiply(num1, num2), "<<")
-
-        elif int(option_selection) == 4:
-            # checks if the second number for divition is 0.
-            if int(num2) == 0:
-                # if the number is devided by 0 the print the error message.
-                print("Can not devide by zero.")
-                continue
-            else:
-                # if not zero then calls the devision funtion and prints the result.
-                print(f" >> {num1} / {num2} =", devide(num1, num2), "<<")
-    # if the provided input is not a numeric value then it prints the error message and keeps the loop running.
-    else:
-        print("Please select a number from the options.")
+            while True:  # initiates a loop to make sure the user provides valid number before moving forward.
+                try:  # checks if the user has provided a valid input in numbers
+                    # asks and converts user input into float from str
+                    num1 = float(input("First Number: "))
+                    while True:  # initiates a loop to make sure the user provides valid number before moving forward.
+                        try:  # initiates a check if the user input is a valid float number for calculations.
+                            # asks for and converts user input into float form str.
+                            num2 = float(input("Second Number: "))
+                            if option_selection == 1:  # if option 1 is selected, performs the add function.
+                                print(f"{num1} + {num2} ={add(num1, num2)}")
+                            # if the option 2 is selected, performs the subtract functions.
+                            elif option_selection == 2:
+                                print(
+                                    f"{num1} - {num2} ={subtract(num1, num2)}")
+                            elif option_selection == 3:  # if option 3 is selected, performs the multipy funtion.
+                                print(
+                                    f"{num1} * {num2} ={multiply(num1, num2)}")
+                            elif option_selection == 4:  # if options 4 is selected, performs the devide function.
+                                try:  # initiates a check to handle the devide by zero error.
+                                    print(
+                                        f"{num1} / {num2} ={devide(num1, num2)}")
+                                except ZeroDivisionError:
+                                    print("Can not devide by zero.")
+                            # breaks the loop initiated for num2 value check.
+                            break
+                        except ValueError:  # handles the value error for num2.
+                            print("Please enter a number.")
+                    break  # breaks the loop initaied for num1
+                except ValueError:  # handles the value error for num1.
+                    print("Please enter a number.")
+        else:
+            print("Please select an option form the menu.")
+    except ValueError:  # completes the check intiated for the option selection menu.
+        print("Please enter a number.")
