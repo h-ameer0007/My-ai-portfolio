50bf283856660fa0ebf54f1718db4d2918f317b8
h-ameer0007 <H.ameer0007@gmail.com>
2025-08-13 12:08:56 +0200

    created main.py file to modulize the operational logic of the utility tool.

---
+++ projects/capestone-projects/week2/cli-file-renamer/main.py
@@ -0,0 +1,4 @@
+# main sctipt for the utility tool operational logic implimentation.
+# Project 2: Command-Line Interface (CLI) File Utilities
+# creates report.txt with all the files and directories in a specifies directory.
+# Objective: Create a Python script that renames files in a specified directory based on a pattern or a new prefix/suffix.
