ee82ac17ed105a5cd06c90669fd9099cce447bbc
h-ameer0007 <H.ameer0007@gmail.com>
2025-08-14 18:30:57 +0200

    added comments to the main.py file to showcase the professional workflow.

--- projects/capestone-projects/week2/cli-file-renamer/main.py
+++ projects/capestone-projects/week2/cli-file-renamer/main.py
@@ -1,5 +1,5 @@
 # main sctipt for the utility tool operational logic implimentation.
-# Project 2: Command-Line Interface (CLI) File Utilities
+# Capestoen Project 2: Command-Line Interface (CLI) File Utilities
 # creates report.txt with all the files and directories in a specifies directory.
 # Objective: Create a Python script that renames files in a specified directory based on a pattern or a new prefix/suffix.
 from utility_ops import report_generator, file_renamer, Safe_duplicate
